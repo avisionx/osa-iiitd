c8e5aa415cb469ddda8f2e86088b77302eebafdc
avisionx <avi17223@iiitd.ac.in>
2021-04-21 11:45:58 +0000

    Deploying to build from  @ c63eb710099367a6892d5bcf5d235e616afcfe6d 🚀

---
+++ precache-manifest.2f910a40eed7a7d02f42ebfdcc022aba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8132a531d49a4bb069d36d66a16c517c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e90b1b80340f117083f",
+    "url": "/static/css/main.789d46f7.chunk.css"
+  },
+  {
+    "revision": "0d4b752f3d8a9a91f40a",
+    "url": "/static/js/2.69e4565f.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.69e4565f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e90b1b80340f117083f",
+    "url": "/static/js/main.1548ce81.chunk.js"
+  },
+  {
+    "revision": "1feede6581793a9e3a29",
+    "url": "/static/js/runtime-main.37390354.js"
+  },
+  {
+    "revision": "8f9f536f91c21eada3825fae91ecb97d",
+    "url": "/static/media/placeholder.8f9f536f.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1ffd17ef5ff972a82305571254bebff4.js"
+  "/precache-manifest.2f910a40eed7a7d02f42ebfdcc022aba.js"
 );
 
 self.addEventListener('message', (event) => {
